66fba3822b40715c09ef6713e47193d9ef224583
CHUN-CHIA <a121515222@gmail.com>
2024-07-19 22:31:07 +0800

    feat:完成初版MainWeatherDisplay排版

--- src/components/MainWeatherDisplay.vue
+++ src/components/MainWeatherDisplay.vue
@@ -1,8 +1,24 @@
 <script setup></script>
 <template>
-  <div class="bg-white p-4 rounded-lg shadow-md flex-1 m-2 h-full">
-    <p class="text-lg">Taipei</p>
-    <p class="text-lg">Weather</p>
-    <p class="text-lg">Wind speed</p>
+  <div class="bg-white p-4 rounded-lg shadow-md flex-1 m-2 h-full flex">
+    <div class="grow m-auto flex items-center justify-center">
+      <div class="h-full">
+        <p class="text-lg">Taipei</p>
+        <p class="text">2024-07-19</p>
+        <p class="text">13:05</p>
+      </div>
+    </div>
+    <div class="h-full grow flex items-center justify-center">
+      <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="" />
+    </div>
+    <div class="grow m-auto">
+      <p class="text-lg text-center h-full">Temperature</p>
+    </div>
+    <div class="grow m-auto">
+      <p class="text-lg text-center h-full">Wind speed</p>
+    </div>
+    <div class="grow m-auto">
+      <p class="text-lg text-center h-full">Humidity</p>
+    </div>
   </div>
 </template>
