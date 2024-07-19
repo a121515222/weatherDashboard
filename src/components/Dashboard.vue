f2e193d44c5c73287e1623b8560dec29f9c5cd3d
CHUN-CHIA <a121515222@gmail.com>
2024-07-19 19:08:31 +0800

    initProject

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from "vue";
+</script>
+
+<template>
+  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
