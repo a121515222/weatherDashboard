b00ecd90b24bfd437e25a0ea3669dad1a3612537
CHUN-CHIA <a121515222@gmail.com>
2024-07-20 06:23:47 +0800

    feat:完成SearchBoard初版元件與AutoCompletet元件

---
+++ src/components/AutoComplete.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps({
+  autoCompleteList: {
+    type: Array,
+    default: () => []
+  }
+});
+const emit = defineEmits(["autoCompleteListEmit"]);
+const emitList = (list) => {
+  emit("autoCompleteListEmit", list);
+};
+</script>
+
+<template>
+  <ul
+    v-show="props.autoCompleteList.length > 0"
+    class="absolute bg-white Relative w-full border border-gray-300 rounded-md shadow-md z-0"
+  >
+    <li
+      v-for="list in props.autoCompleteList"
+      class="hover:bg-gray-100 text-color-800 p-2 cursor-pointer"
+      @click="emitList(list)"
+    >
+      {{ list }}
+    </li>
+  </ul>
+</template>
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,8 +2,8 @@
 import { ref } from "vue";
 import MainWeatherDisplayVue from "./MainWeatherDisplay.vue";
 import ForecastWeatherDisplayVue from "./ForecastWeatherDisplay.vue";
+import SearchBoardVue from "./SearchBoard.vue";
 import { useDashBoardStore } from "@/stores/dashBoardStore";
-
 import { storeToRefs } from "pinia";
 const { forecasts } = storeToRefs(useDashBoardStore());
 const weather = ref(null);
@@ -18,49 +18,7 @@ const weather = ref(null);
           <MainWeatherDisplayVue></MainWeatherDisplayVue>
         </div>
         <div class="col-span-12 lg:col-span-4">
-          <div class="bg-white p-4 rounded-lg shadow-md flex-1 m-2 h-full">
-            <div class="flex items-center mb-4">
-              <input
-                type="text"
-                placeholder="Search city"
-                class="border p-2 flex-grow mr-2 rounded"
-                v-model="searchCity"
-              />
-              <button
-                class="bg-blue-500 text-white p-2 rounded"
-                @click="search"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M21 21l-4.35-4.35m2.35-4.65a7 7 0 11-14 0 7 7 0 0114 0z"
-                  />
-                </svg>
-              </button>
-            </div>
-            <div class="flex justify-between">
-              <button
-                class="border p-2 w-1/2 mr-1 rounded bg-orange-400"
-                @click="setUnit('F')"
-              >
-                F°
-              </button>
-              <button
-                class="border p-2 w-1/2 ml-1 rounded bg-lime-400"
-                @click="setUnit('C')"
-              >
-                C°
-              </button>
-            </div>
-          </div>
+          <SearchBoardVue />
         </div>
       </div>
 
