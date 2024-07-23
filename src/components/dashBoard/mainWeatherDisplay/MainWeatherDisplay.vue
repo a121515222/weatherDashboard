4e6a0322b24c7d713f67130ce30ed221fc24ccaf
CHUN-CHIA <a121515222@gmail.com>
2024-07-23 15:37:02 +0800

    feat:mainWeatherDisplay背景色隨著天氣改變

--- src/components/dashBoard/mainWeatherDisplay/MainWeatherDisplay.vue
+++ src/components/dashBoard/mainWeatherDisplay/MainWeatherDisplay.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { computed } from "vue";
+import imgSvg from "@/assets/img.svg";
 const props = defineProps({
   currentCityWeatherProp: {
     type: Object,
@@ -14,10 +16,38 @@ const props = defineProps({
     }
   }
 });
-import imgSvg from "@/assets/img.svg";
+const backgroundClass = computed(() => {
+  const weatherCodeColorsClass = {
+    "01d": "bg-gradient-to-r from-sky-500 to-indigo-500", // Clear sky day
+    "02d": "bg-gradient-to-r from-sky-400 to-indigo-400", // Few clouds day
+    "03d": "bg-gradient-to-r from-sky-300 to-indigo-400", // Scattered clouds day
+    "04d": "bg-gradient-to-r from-sky-400 to-gray-500", // Broken clouds day
+    "50d": "bg-gradient-to-r from-gray-500 to-gray-600", // Mist day
+    "09d": "bg-gradient-to-r from-gray-400 to-sky-600", // Shower rain day
+    "10d": "bg-gradient-to-r from-gray-400 to-sky-500", // Rain day
+    "13d": "bg-gradient-to-r from-gray-500 to-sky-400", // Snow day
+    "11d": "bg-gradient-to-r from-gray-500 to-gray-700", // Thunderstorm day
+    "01n": "bg-gradient-to-r from-slate-700 to-slate-800", // Clear sky night
+    "02n": "bg-gradient-to-r from-slate-600 to-slate-700", // Few clouds night
+    "03n": "bg-gradient-to-r from-slate-500 to-slate-600", // Scattered clouds night
+    "04n": "bg-gradient-to-r from-slate-600 to-gray-500", // Broken clouds night
+    "50n": "bg-gradient-to-r from-gray-600 to-gray-700", // Mist night
+    "09n": "bg-gradient-to-r from-gray-400 to-slate-800 ", // Shower rain night
+    "10n": "bg-gradient-to-r from-gray-400 to-slate-700", // Rain night
+    "13n": "bg-gradient-to-r from-gray-500 to-slate-600", // Snow night
+    "11n": "bg-gradient-to-r from-gray-700 to-gray-800" // Thunderstorm night
+  };
+  const weatherCode = props.currentCityWeatherProp?.weatherCode;
+  return weatherCodeColorsClass[weatherCode]
+    ? `${weatherCodeColorsClass[weatherCode]} text-white`
+    : "bg-white text-black"; // default color
+});
 </script>
 <template>
-  <div class="bg-white p-4 rounded-lg shadow-md flex-1 m-2 h-full flex">
+  <div
+    :class="backgroundClass"
+    class="p-4 rounded-lg shadow-md flex-1 m-2 h-full flex"
+  >
     <div class="grow m-auto flex items-center justify-center">
       <div class="h-full">
         <p class="text-2xl">
